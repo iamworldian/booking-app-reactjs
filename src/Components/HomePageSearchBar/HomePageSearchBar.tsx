6a65f3b7651d5b65de4b39861555f4f535938f88
Mohammed Ashiqur Rahman Baig <ashiqur1107@gmail.com>
2023-06-02 02:45:09 +0600

    updates

--- src/Components/HomePageSearchBar/HomePageSearchBar.tsx
+++ src/Components/HomePageSearchBar/HomePageSearchBar.tsx
@@ -4,9 +4,7 @@ import Style from './HomePageSearchBar.module.css';
 export default function HomePageSearchBar() {
   return (
     <div className={classJoiner(Style, ['container'])}>
-      <div className={classJoiner(Style, ['searchGroup'])}>
-            
-      </div>
+      <div className={classJoiner(Style, ['searchGroup'])}></div>
     </div>
   );
 }
