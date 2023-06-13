e703c73390fac9972e570e974651e733b2e86c40
Mohammed Ashiqur Rahman Baig <ashiqur1107@gmail.com>
2023-06-13 19:50:05 +0600

    update components

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,12 +1,24 @@
+import './HomePage.css';
+
+import Featured from '../Components/Featured/Featured';
 import HomePageBanner from '../Components/HomePageBanner/HomePageBanner';
 import HomePageHeader from '../Components/HomePageHeader/HomePageHeader';
 import HomePageSearchBar from '../Components/HomePageSearchBar/HomePageSearchBar';
+
 export default function HomePage() {
   return (
     <>
       <HomePageHeader />
       <HomePageBanner />
       <HomePageSearchBar />
+
+      <div className="featuredDiv">
+        <Featured />
+      </div>
+
+      <div className="featuredDiv">
+        <Featured />
+      </div>
     </>
   );
 }
